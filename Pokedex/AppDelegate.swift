a9170fb011aa1207b3ff4e6aec28ab5c36668a52
brusc22 <marcobruscolini@gmail.com>
2019-06-25 19:49:39 +0200

    Update AppDelegate.swift

--- Pokedex/AppDelegate.swift
+++ Pokedex/AppDelegate.swift
@@ -1,4 +1,4 @@
-Numero simulazioni: 100000
+Numero simulazioni: 500000
 Strike price: 195.0
 Prezzo opzione con formula di Black & Scholes: 12.3375220648
-Prezzo opzione con metodo Monte Carlo: 12.1177227542
+Prezzo opzione con metodo Monte Carlo: 12.2912789353
