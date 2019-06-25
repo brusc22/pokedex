b3a5f2c22d11dbf3c3af841e06fa4e3e82e5b413
brusc22 <marcobruscolini@gmail.com>
2019-06-25 17:55:09 +0200

    Update AppDelegate.swift

--- Pokedex/AppDelegate.swift
+++ Pokedex/AppDelegate.swift
@@ -1,6 +1,4 @@
-def pure_Monte_Carlo():
-        for t in range(1, n_steps):
-            rand = numpy.random.standard_normal(N)
-            S[t] = S[t - 1] * numpy.exp((r - 0.5 * std ** 2) * deltaT + (std * numpy.sqrt(deltaT) * rand))
-        C = numpy.exp(-r * T)  * numpy.sum(numpy.maximum(S[-1] - K, 0)) * (1 / N)
-        return C
+Numero simulazioni: 100000
+Strike price: 195
+Prezzo opzione con formula di Black & Scholes: 12.3375220648
+Prezzo opzione con metodo Monte Carlo: 12.3119154149
