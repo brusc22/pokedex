6f22e18036f7d925008294d9f7444c7e2e98401f
brusc22 <marcobruscolini@gmail.com>
2019-06-26 12:28:44 +0200

    Update AppDelegate.swift

--- Pokedex/AppDelegate.swift
+++ Pokedex/AppDelegate.swift
@@ -1,16 +1,4 @@
-def antithetic_Monte_Carlo():
-    n = int(N/2)
-    S1 = S[:,0:n]
-    S2 = S[:,0:n]
-
-    for t in range(1, n_steps):
-        rand1 = numpy.random.standard_normal(n)
-        rand2 = -rand1
-        S1[t] = S1[t - 1] * numpy.exp((r - 0.5 * std ** 2) * deltaT + (std * numpy.sqrt(deltaT) * rand1))
-        S2[t] = S2[t - 1] * numpy.exp((r - 0.5 * std ** 2) * deltaT + (std * numpy.sqrt(deltaT) * rand2))
-
-    STot = numpy.concatenate((S1,S2), axis=1)
-
-    C = numpy.exp(-r * T) * numpy.sum(numpy.maximum(STot[-1] - K, 0)) * (1 / N)
-
-    return C
+Numero simulazioni: 100000
+Strike price: 195.0
+Prezzo opzione con formula di Black & Scholes: 12.3375220648
+Prezzo opzione con metodo Monte Carlo (stimatore antitetico): 12.3847870764
